5ccfa3c72a28acfa530552a39b48553b805861ce
ahumyck <ahumyck@gmail.com>
2020-05-22 05:20:18 +0400

    code refactor

--- nirs.py
+++ nirs.py
@@ -24,7 +24,7 @@ W = alpha * G + beta * H
         
 
 
-# In[46]:
+# In[2]:
 
 
 def generate_public_shares(Qs, Rs):
@@ -99,7 +99,6 @@ A = threshold_matrix_generation(t, n)
 numbers = generate_pairings_numbers(t, l)
 Ps = transform_to_points(numbers)
 P = Ps[-1]
-print(Ps[-1])
 user_points = compute_shares(A, numbers) #giveaway to Users
 Qs = transform_to_points(generate_pairings_numbers(t, l))
 shares_as_points = pairings(Qs,P,W)
@@ -107,14 +106,8 @@ Rs = add(shares_as_points, secrets)
 
 public_information = generate_public_shares(Qs,Rs)
 
-# print("matrix")
-# print(A)
 print("secrets = {}".format(secrets))
-print("numbers = {}".format(numbers))
 print("user_points = {}".format(user_points))
-print("Ps = {}".format(Ps))
-print("Qs = {}".format(Qs))
-# print("Rs = {}".format(Rs))
 
 print("\npublic")
 for info in public_information:
@@ -122,7 +115,7 @@ for info in public_information:
 
 
 
-# In[47]:
+# In[3]:
 
 
 def subtract(Rs, Ts):
@@ -130,11 +123,6 @@ def subtract(Rs, Ts):
     for R, T in zip(Rs, Ts):
         res.append(R - T)
     return res
-    
-
-def invert_matrix(A, rows):
-    print(A.matrix_from_rows(rows))
-    return A.matrix_from_rows(rows).inverse()
 
 def calculate_S(Qs, UPs, W, rows):
     S = []
@@ -145,89 +133,63 @@ def calculate_S(Qs, UPs, W, rows):
         S.append(tmp)
     return S
 
-
-def calculate_T(Ss, A):
-    rows = A.rows()
-    Ts = []
-    for i in range(len(rows)):
-        T = int(rows[-1][0]) * Ss[i][0]
-        for j in range(1, len(rows[i]), 1):
-            T += int(rows[-1][j]) * Ss[i][j]
-        Ts.append(T)
-    return Ts
-
-"""
-    Each of them can calculate {y(k)}
-    and use it to restore ~P(t), so instead
-    we can calculate pairing e(i) = (Q(i), ~P(t)) to get our secret back
-    with following formula : S(i) = R(i) - e(i)
-"""
-
-
-rows = [0, 1]
+rows = [0, 2]
 pseudo_shares = calculate_S(Qs, user_points, W, rows)
-inv = invert_matrix(A, rows)
-print(inv)
-Ts = calculate_T(pseudo_shares, inv)
-recovered_secrets = subtract(Rs, Ts)
-print("secrets = {}".format(recovered_secrets))
     
 
 
-# In[60]:
+# In[4]:
 
 
-print("user_points = {}".format(user_points))
-print("public")
-for info in public_information:
-    print(info)
-# print(numbers)
-_a, _b = matrix([numbers[0]]), matrix([numbers[1]])
-_a = _a.transpose()
-_b = _b.transpose()
-a = A * _a
-b = A * _b
-print("rows = {}".format(rows))
-print(_a)
-print(_b)
-# print()
-print(a.column(0))
-print(b.column(0))
+def calculate_j_symbol(u, j):
+    res = 1
+    for i in range(len(u)):
+        if i == j:
+            continue
+        else:
+            res *= (u[j] - u[i])
+    return 1/res
 
-def v(a, b, rows):
-    a1 = []
-    b1 = []
+
+def calculate_last_matrix_row(u):
+    y = []
+    for i in range(len(u)):
+        y.append(calculate_j_symbol(u, i))
+    return y
+
+def restore_last_point(last_matrix_row, user_points, rows):
+    x, y = 0, 0
     for i in range(len(rows)):
-        a1.append(a.column(0)[rows[i]])
-        b1.append(b.column(0)[rows[i]])
-    
-    return matrix(a1), matrix(b1)
+        a, b = user_points[rows[i]]
+        x += last_matrix_row[i] * a
+        y += last_matrix_row[i] * b
+    return x, y
 
-print()
-print(inv)
-print()
-aa, bb = v(a, b, rows)
-aaa = aa * inv
-bbb = bb * inv
-print(aaa)
-print(bbb)
 
-pppp = (int(aaa.row(0)[1]), int(bbb.row(0)[1]))
-print(pppp)
-Q1 = (public_information[0][0], public_information[0][1])
-Q2 = (public_information[1][0], public_information[1][1])
-print(Q1, Q2)
-e1 = pairing(Q1, pppp, W)
-e2 = pairing(Q2, pppp, W)
+def get_coefficients_from_public_information(public_information):
+    qs = []
+    for info in public_information:
+        qs.append((info[0], info[1]))
+    return qs
 
-print(shares_as_points)
+def get_points_from_public_information(public_information):
+    rs = []
+    for info in public_information:
+        rs.append(info[-1])
+    return rs
 
-# print(e1)
-# print(e2)
 
-print(Rs[0] - e1)
-print(Rs[1] - e2)
-        
+last_matrix_row = calculate_last_matrix_row(rows)
+pppp = restore_last_point(last_matrix_row, user_points, rows)
+print("expected ~P(t) =", P)
+print("~actual  ~P(t) =", pppp)
+if P == pppp:
+    qqqq = get_coefficients_from_public_information(public_information)
+    rrrr = get_points_from_public_information(public_information)
+    eeee = pairings(qqqq, P, W)
+    ssss = subtract(rrrr, eeee)
+    print(ssss)
+       
 
 
 # In[ ]:
